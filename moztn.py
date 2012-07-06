1ed4f74bc626d17fe1aef14067dd1c67b668c8f4
Alexandre BM <rednaks@pro-programming.net>
2012-07-06 08:19:18 +0100

    able to accept invitation and say hello !

--- moztn.py
+++ moztn.py
@@ -10,12 +10,12 @@ import time
 #HOST=sys.argv[1]
 PORT=6667
 #NICK=sys.argv[2]
-HOST='irc.recycled-irc.net'
+HOST='irc.mozilla.org'
 NICK='moztn'
 IDENT="Mozilla Tunisia Bot"
 REALNAME="moztnBot"
 #readbuffer=""
-CHANNEL = '#esprit-libre'
+#CHANNEL = '#esprit-libre'
 
 cmd_list=['quit','names','join']
 
@@ -23,7 +23,21 @@ s=socket.socket( )
 s.connect((HOST, PORT))
 s.send("NICK %s\r\n" % NICK)
 s.send("USER %s %s bla :%s\r\n" % (IDENT, HOST, REALNAME))
-s.send("JOIN %s\r\n" % CHANNEL)
+#s.send("JOIN %s\r\n" % CHANNEL)
+
+
+
+def joinChannel(msg):
+  channel = GetChannel(msg)
+  s.send("JOIN %s\r\n" % channel)
+
+def MakeAction(msg):
+  if(msg.find('PRIVMSG') is not -1):
+     return 1
+  elif(msg.find('INVITE') is not -1):
+     return 2
+  else:
+     return 0
 
 def GetMsg(msg):
   msg = msg[msg.find('PRIVMSG'):]
@@ -33,8 +47,17 @@ def GetMsg(msg):
 
 def GetUname(msg):
   return msg[msg.find(':')+1:msg.find('!')]
+
+def GetChannel(msg):
+  if(msg.find('INVITE')>-1):
+    return msg[msg.find('#'):msg.find('\r')]
+  elif(msg.find('PRIVMSG')>-1):
+    msg = msg[msg.find('#'):]
+    return msg[:msg.find(':')-1]
   
 def printMsg(msg):
+  if(msg.find(' hi') >-1 or msg.find('hello') >-1 or msg.find('Hello') > -1):
+    s.send("PRIVMSG %s :Hello %s :) How are you ? How can I help you ?\r\n" % (GetChannel(msg),GetUname(msg)))
   print '@'+GetUname(msg)+': '+GetMsg(msg)
 
 class PingThread(threading.Thread):
@@ -48,9 +71,13 @@ class PingThread(threading.Thread):
     readbuffer=readbuffer+s.recv(1024)
     temp=string.split(readbuffer, "\n")
     readbuffer=temp.pop( )
-    #print temp
+    print temp
+    if(MakeAction(temp[0]) is 1):
+        printMsg(temp[0])
+    elif(MakeAction(temp[0]) is 2):
+        joinChannel(temp[0])
+
     #print GetMsg(temp[0])
-    printMsg(temp[0])
     for line in temp:
         line=string.rstrip(line)
         line=string.split(line)
