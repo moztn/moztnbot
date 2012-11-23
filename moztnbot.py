41366c5af86bc2a08847619c33a46a99e4d40288
Alexandre BM <s@rednaks.tn>
2012-11-23 21:57:25 +0100

    rename to moztnbot

--- moztnbot.py
+++ moztnbot.py
@@ -61,7 +61,7 @@ class Message:
 	def log(self):
 		date = getDate()
 		channel = self.GetChannel().replace('#','')
-		fname = 'log/moztnbot-'+channel+'-'+date+'.log.html'
+		fname = '/var/www/irc/log/'+channel+'-'+date+'.log.html'
 		time = getTime()
 		uname = self.GetUname()
 		msg = self.GetMsg()
@@ -76,6 +76,15 @@ class Message:
 			content = '		<p>'+'<span class="time">'+time+'</span> <span class="nick">'+uname+'</span> '+msg+'</p>\n'
 			f.write(content.encode('utf-8'))
 			f.close()
+	def pushLog(self):
+		try:
+			date = getDate()
+			channel = self.GetChannel().replace('#','')
+			fname = channel+'-'+date+'.log.html'
+			return 'http://irc.mozilla-tunisia.org/log/'+date+'/'+fname
+		except:	
+			return None
+
 
 #Config
 config = {}
@@ -143,6 +152,12 @@ def MakeAction(msg):
 			s.send("PRIVMSG %s :%s, %s\r\n" % (message.GetChannel(),message.GetUname(),"Type \x02!help\x02 to learn more.")) 
 		if(message.contains('!help')):
 			s.send("PRIVMSG %s :%s, Sorry, my master is too lasy to implement this :( you can maybe help on https://github.com/rednaks/EspritLibreBot ?\r\n" % (message.GetChannel(),message.GetUname()))
+		if(message.contains('!log')):
+			url = message.pushLog()
+			if(url is not None):
+				s.send("PRIVMSG %s :%s you can find the log here : %s\r\n" % (message.GetChannel(),message.GetUname(),url))
+			else:
+				s.send("PRIVMSG %s :%s Sorry unable to get log please contact my master\r\n" % (message.GetChannel(), message.GetUname()))
 	if(message.contains('INVITE')):
 		joinChannel(msg)
 
@@ -157,7 +172,7 @@ def main_loop():
   readbuffer=readbuffer+s.recv(1024)
   temp=string.split(readbuffer, "\n")
   readbuffer=temp.pop()
-#  print temp
+  print temp
   if(temp[0].find(linkname) is -1):
     try:
       MakeAction(temp[0].decode('utf-8'))
